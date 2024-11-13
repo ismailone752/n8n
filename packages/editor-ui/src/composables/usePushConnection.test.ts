6c544e02a57ce58971781d736b62dea85489835a
Ivan Atanasov <ivan.atanasov@n8n.io>
2024-11-13 16:22:24 +0100

    adding tests for error message

--- packages/editor-ui/src/composables/usePushConnection.test.ts
+++ packages/editor-ui/src/composables/usePushConnection.test.ts
@@ -5,6 +5,9 @@ import type { PushMessage, PushPayload } from '@n8n/api-types';
 import { usePushConnection } from '@/composables/usePushConnection';
 import { usePushConnectionStore } from '@/stores/pushConnection.store';
 import { useOrchestrationStore } from '@/stores/orchestration.store';
+import { useUIStore } from '@/stores/ui.store';
+import { useWorkflowsStore } from '@/stores/workflows.store';
+import { useToast } from '@/composables/useToast';
 
 vi.mock('vue-router', () => {
 	return {
@@ -16,6 +19,19 @@ vi.mock('vue-router', () => {
 	};
 });
 
+vi.mock('@/composables/useToast', () => {
+	const showMessage = vi.fn();
+	const showError = vi.fn();
+	return {
+		useToast: () => {
+			return {
+				showMessage,
+				showError,
+			};
+		},
+	};
+});
+
 vi.useFakeTimers();
 
 describe('usePushConnection()', () => {
@@ -23,6 +39,8 @@ describe('usePushConnection()', () => {
 	let pushStore: ReturnType<typeof usePushConnectionStore>;
 	let orchestrationStore: ReturnType<typeof useOrchestrationStore>;
 	let pushConnection: ReturnType<typeof usePushConnection>;
+	let uiStore: ReturnType<typeof useUIStore>;
+	let workflowsStore: ReturnType<typeof useWorkflowsStore>;
 
 	beforeEach(() => {
 		setActivePinia(createPinia());
@@ -30,6 +48,8 @@ describe('usePushConnection()', () => {
 		router = vi.mocked(useRouter)();
 		pushStore = usePushConnectionStore();
 		orchestrationStore = useOrchestrationStore();
+		uiStore = useUIStore();
+		workflowsStore = useWorkflowsStore();
 		pushConnection = usePushConnection({ router });
 	});
 
@@ -104,6 +124,87 @@ describe('usePushConnection()', () => {
 
 				expect(spy).toHaveBeenCalledWith(event.data.status);
 				expect(result).toBeTruthy();
+			});
+		});
+
+		describe('executionFinished', () => {
+			it('should handle executionFinished event correctly', async () => {
+				const event: PushMessage = {
+					type: 'executionFinished',
+					data: {
+						executionId: '1',
+						data: {
+							data: {
+								runData: {},
+								error: null,
+								lastNodeExecuted: 'testNode',
+								resultData: {},
+							},
+							finished: true,
+							mode: 'manual',
+							startedAt: new Date(),
+							stoppedAt: new Date(),
+						},
+					},
+				};
+
+				workflowsStore.activeExecutionId = '1';
+				uiStore.isActionActive['workflowRunning'] = true;
+
+				const result = await pushConnection.pushMessageReceived(event);
+
+				expect(result).toBeTruthy();
+				expect(workflowsStore.workflowExecutionData).toBeDefined();
+				expect(uiStore.isActionActive['workflowRunning']).toBeTruthy();
+
+				expect(useToast().showMessage).toHaveBeenCalledWith({
+					title: 'Workflow executed successfully',
+					type: 'success',
+				});
+			});
+
+			it('should handle isManualExecutionCancelled correctly', async () => {
+				const event: PushMessage = {
+					type: 'executionFinished',
+					data: {
+						executionId: '1',
+						data: {
+							data: {
+								startData: {},
+								resultData: {
+									runData: {
+										'Last Node': [],
+									},
+									lastNodeExecuted: 'Last Node',
+									error: {
+										message:
+											'Your trial has ended. <a href="https://app.n8n.cloud/account/change-plan">Upgrade now</a> to keep automating',
+									},
+								},
+							},
+							mode: 'manual',
+							status: 'running',
+						},
+					},
+				};
+
+				workflowsStore.activeExecutionId = '1';
+				uiStore.isActionActive['workflowRunning'] = true;
+
+				const result = await pushConnection.pushMessageReceived(event);
+
+				expect(useToast().showMessage).toHaveBeenCalledWith({
+					message:
+						'Your trial has ended. <a href="https://app.n8n.cloud/account/change-plan">Upgrade now</a> to keep automating',
+					title: 'Problem in node ‘Last Node‘',
+					type: 'error',
+					duration: 0,
+					dangerouslyUseHTMLString: true,
+				});
+
+				expect(result).toBeTruthy();
+				expect(workflowsStore.workflowExecutionData).toBeDefined();
+				expect(uiStore.isActionActive['workflowRunning']).toBeTruthy();
 			});
 		});
 	});
